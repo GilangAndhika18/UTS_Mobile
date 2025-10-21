0309e6d306bc90324a4fdc21d3523c6658fe9d1c
Gilang Andhika Erlangga <gilangandhikaerlangga18@gmail.com>
2025-10-21 17:21:28 +0700

    UTS: Selesai Soal 1 - ListTile dan Navigasi Balik

---
+++ ocr_sederhana/lib/screens/result_screen.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'home_screen.dart';
+
+class ResultScreen extends StatelessWidget {
+  final String ocrText;
+
+  const ResultScreen({super.key, required this.ocrText});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Hasil OCR')),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: SingleChildScrollView(
+          child: SelectableText(
+            ocrText.isEmpty
+                ? 'Tidak ada teks ditemukan.'
+                : ocrText, // ⬅️ Hapus replaceAll agar baris baru tetap tampil
+            style: const TextStyle(fontSize: 18),
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.pushAndRemoveUntil(
+            context,
+            MaterialPageRoute(builder: (_) => const HomeScreen()),
+            (route) => false, // ⬅️ Menghapus semua halaman sebelumnya
+          );
+        },
+        child: const Icon(Icons.home),
+      ),
+    );
+  }
+}
